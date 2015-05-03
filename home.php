826c20826ff7af58722525005fe1d6ab8a120961
Shine Shoothanasoonthorn <shine.egco@gmail.com>
2015-05-03 22:18:58 +0700

    Add 3 Photo Banner

--- home.php
+++ home.php
@@ -106,13 +106,13 @@
                         </ol>
                         <div class="carousel-inner">
                             <div class="item active">
-                                <img class="slide-image" src="http://placehold.it/800x300" alt="">
+                                <img class="slide-image" src="image/banner1.png" alt="">
                             </div>
                             <div class="item">
-                                <img class="slide-image" src="http://placehold.it/800x300" alt="">
+                                <img class="slide-image" src="image/banner2.png" alt="">
                             </div>
                             <div class="item">
-                                <img class="slide-image" src="http://placehold.it/800x300" alt="">
+                                <img class="slide-image" src="image/banner3.png" alt="">
                             </div>
                         </div>
                         <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
