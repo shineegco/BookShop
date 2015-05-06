3b23668fcb4ef3d19f10502edc2aaf3260240bc9
Kewalee <Kewalee@Tangmo>
2015-05-06 17:54:37 +0700

    change check login.

--- include/login.inc.php
+++ include/login.inc.php
@@ -18,11 +18,11 @@
 	
 	} else { 
 	
-	 // Make sure that user submitted a username/password and username only consists of alphanumeric chars 
+                // Make sure that user submitted a username/password 
 		
-		 if ( (!isset($_POST['username'])) || (!isset($_POST['password'])) OR (!ctype_alnum($_POST['username'])) ) { 
+		 if ( (!isset($_POST['username'])) || (!isset($_POST['password'])) ) { 
 			
-		 	//redirect('../login.php'); 
+		 	redirect('../login.php'); 
 	
 		 } 
 		
@@ -36,50 +36,49 @@
 		
 		 	printf("Unable to connect to database: %s", mysqli_connect_error()); 
 		
-		 exit(); 
-	
-	 }
+                        exit(); 
+                }
 	 
-	 // Escape any unsafe characters before querying database 
-	 $username = $mysqli->real_escape_string($_POST['username']); 
-	 $password = $mysqli->real_escape_string($_POST['password']); 
-	
-	 // Construct SQL statement for query & execute 
-	 $sql = "SELECT * FROM username WHERE username = '" . $username . "' AND password = '" . md5($password) . "'"; 
-	
-	 $result = $mysqli->query($sql); 
-	
-	 // If one row is returned, username and password are valid 
-	
-	 if (is_object($result) && $result->num_rows == 1) { 
-	
-		 // Set session variable for login status to true 
-		$_SESSION['username'] = $username;
-                
-                echo "username  ".$_SESSION['username']."<br>";////////////try//////////
-                
-                $row = mysqli_fetch_array($result);
-                
-                
-                $_SESSION['uid'] = $row['id'];
-                
-                echo "id  ".$_SESSION['uid']."<br>";////////////try//////////               
-		
-		$_SESSION['logged_in'] = true; 
-                
-                echo "logged_in  ".$_SESSION['logged_in']."<br>";////////////try//////////   
+                // Escape any unsafe characters before querying database 
+                $username = $mysqli->real_escape_string($_POST['username']); 
+                $password = $mysqli->real_escape_string($_POST['password']); 
 
-		setcookie("uid", $row['id'], time() + (86400 * 30), "/");
-		
-		redirect('../home.php'); 
-	
-	 } else { 
-	
-		 // If number of rows returned is not one, redirect back to login screen 
-		
-		 //redirect('../login.php'); 
-	
-	 } 
+                // Construct SQL statement for query & execute 
+                $sql = "SELECT * FROM username WHERE username = '" . $username . "' AND password = '" . md5($password) . "'"; 
+
+                $result = $mysqli->query($sql); 
+
+                // If one row is returned, username and password are valid 
+
+                if (is_object($result) && $result->num_rows == 1) { 
+
+                        // Set session variable for login status to true 
+                       $_SESSION['username'] = $username;
+
+                       echo "username  ".$_SESSION['username']."<br>";////////////try//////////
+
+                       $row = mysqli_fetch_array($result);
+
+
+                       $_SESSION['uid'] = $row['id'];
+
+                       echo "id  ".$_SESSION['uid']."<br>";////////////try//////////               
+
+                       $_SESSION['logged_in'] = true; 
+
+                       echo "logged_in  ".$_SESSION['logged_in']."<br>";////////////try//////////   
+
+                       setcookie("uid", $row['id'], time() + (86400 * 30), "/");
+
+                       redirect('../home.php'); 
+
+                } else { 
+
+                        // If number of rows returned is not one, redirect back to login screen 
+
+                        redirect('../login.php'); 
+
+                } 
 	
 	} 
 
